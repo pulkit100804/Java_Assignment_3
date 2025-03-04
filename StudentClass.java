210d288423ab8b72aeb4721ac87a7b6e0faf5d7b
Pulkit Shrivastava <167713133+pulkit100804@users.noreply.github.com>
2025-03-04 21:21:14 +0530

    Update StudentClass.java

--- StudentClass.java
+++ StudentClass.java
@@ -1,1 +1,13 @@
-
+public class Student {
+    String prn;
+    String name;
+    int marks;
+    public Student(String prn, String name, int marks) {
+        this.prn = prn;
+        this.name = name;
+        this.marks = marks;
+    }
+    public void display() {
+        System.out.println("PRN: " + prn + ", Name: " + name + ", Marks: " + marks);
+    }
+}
